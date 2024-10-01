bfca1c8f113b0988582783565c42f1bb82db1ea0
John Nelson <sigoofballde@gmail.com>
2024-10-01 09:15:48 -0500

    Updating readme, removing previous AI API connections

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # reply-with-quotes-api
 
-### .env file variables
+## .env file variables
 
 - PORT=7069
   - This port should not be changed unless you change UI side.
 - GEMINI_API_KEY='Your API Key here'
+
+## Run locally
+
+- npm start
--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -6,61 +6,12 @@ import { Request, Response } from 'express'
 import { parseQuotes } from '../utils/dataManipulation'
 import { Quote } from '../interfaces/general'
 import { emptyDataSet } from '../utils/errors'
-import { inspect } from 'util'
 
-// const API_KEY = process.env.ANTHROPIC_API_KEY
 const API_KEY = process.env.GEMINI_API_KEY
 
 export const home = (req: Request, res: Response) => {
   res.json({ response: 'API is up and running!' })
 }
-
-/**OPENAI CHATGPT TRY */
-// app.post('/getConversation', async (req: Request, res: Response) => {
-//     const { body } = req
-// const client = new OpenAI({
-//     organization: process.env.OPENAI_ORGANIZATION_ID,
-//     project: process.env.OPENAI_PROJECT_ID,
-//     apiKey: process.env.OPENAI_SECRET_KEY,
-// })
-//     try {
-//         // const completion = await client.chat.completions.create({
-//         //     model: 'gpt-4o-mini',
-//         //     messages: [{ 'role': 'user', 'content': `give me the 3 best movie or tv show quotes to reply to "${body}"` }]
-//         // })
-//         res.status(200).json(completion)
-//     } catch (e) {
-//         res.sendStatus(500)
-//     }
-// })
-
-/**ANTHROPIC CLAUDE 3 TRY */
-// app.post('/getConversation', async (req: Request, res: Response) => {
-//   const { conversation } = req.body
-//   const prompt = `give me the 3 best movie or tv show quotes to reply to "${conversation}"`
-// interface ClaudeResponse {
-//   completion: string
-// }
-
-// const anthropic = new Anthropic({
-//   apiKey: API_KEY,
-// })
-//   try {
-//     const completion = await anthropic.messages.create({
-//       model: 'claude-3-5-sonnet-20240620',
-//       max_tokens: 1024,
-//       messages: [
-//         {
-//           role: 'user',
-//           content: prompt,
-//         },
-//       ],
-//     })
-//     res.status(200).json(completion)
-//   } catch (e: any) {
-//     res.sendStatus(500)
-//   }
-// })
 
 /**GOOGLE GEMINI TRY */
 export const getConversation = async (req: Request, res: Response) => {
